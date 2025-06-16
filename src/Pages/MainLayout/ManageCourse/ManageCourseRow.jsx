b40431254e19e91d4a936b2f4f2b373a5980e000
soponalways <soponalways@gmail.com>
2025-06-16 22:14:47 +0600

    delete the course

--- src/Pages/MainLayout/ManageCourse/ManageCourseRow.jsx
+++ src/Pages/MainLayout/ManageCourse/ManageCourseRow.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router';
 
-const ManageCourseRow = ({course, index}) => {
+const ManageCourseRow = ({ course, index, handleDeleteCourse }) => {
     const {courseTitle, shortDescription, _id, slug , adminEmail} = course || {}; 
+    
     return (
         <tr>
             <th>{index + 1}</th>
@@ -10,7 +11,7 @@ const ManageCourseRow = ({course, index}) => {
             <td>{shortDescription}</td>
             <td className='flex gap-1 md:gap-2 lg:gap-3'>
                 <Link to={`/editCourse/?slug=${slug}&email=${adminEmail}&course_id=${_id}`} className='btn btn-secondary hover:btn-accent focus:btn-active btn-sm'>Edit</Link>
-                <button className='btn btn-secondary hover:btn-accent focus:btn-active btn-sm'>Delete</button>
+                <button onClick={() => handleDeleteCourse(adminEmail, _id)} className='btn btn-secondary hover:btn-accent focus:btn-active btn-sm'>Delete</button>
             </td>
         </tr>
     );
--- src/Pages/MainLayout/ManageCourse/ManageCurseList.jsx
+++ src/Pages/MainLayout/ManageCourse/ManageCurseList.jsx
@@ -1,10 +1,45 @@
-import React, { use } from 'react';
+import React, { use, useState } from 'react';
 import ManageCourseRow from './ManageCourseRow';
+import useAxiosSecure from '../../../hooks/useAxiosSecure';
+import { toast } from 'react-toastify';
+import Swal from 'sweetalert2';
 
 const ManageCurseList = ({ getCouresesByEmail }) => {
-    const courses = use(getCouresesByEmail)
-    console.log('data from the server ' , courses)
-    console.log(getCouresesByEmail)
+    const axiosSecure = useAxiosSecure();
+    const [courses, setCourses] = useState(use(getCouresesByEmail))
+
+    const handleDeleteCourse = (adminEmail, _id) => {
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+        }).then((result) => {
+            if (result.isConfirmed) {
+                axiosSecure.delete(`/course/${adminEmail}/${_id}`)
+                    .then(res => {
+                        const data = res.data;
+                        if (data.deletedCount) {
+                            const remainingCourse = courses.filter(course => course._id !== _id); 
+                            setCourses(remainingCourse); 
+                            
+                            Swal.fire({
+                                title: "Deleted!",
+                                text: "These course has been successfully Delete",
+                                icon: "success"
+                            });
+                        }
+                    }).catch(error => {
+                        console.log(error)
+                        toast.warning(error.message);
+                    })
+            }
+        });
+    }
+
     return (
         <div className="overflow-x-auto rounded-box border border-base-content/5 bg-base-100">
             <table className="table">
@@ -20,7 +55,12 @@ const ManageCurseList = ({ getCouresesByEmail }) => {
                 <tbody>
                     {/* rows are here */}
                     {
-                        courses?.map((course, index) => <ManageCourseRow course={course} index={index} key={index}></ManageCourseRow>)
+                        courses?.map((course, index) => <ManageCourseRow
+                            course={course}
+                            index={index}
+                            key={index}
+                            handleDeleteCourse={handleDeleteCourse}
+                        ></ManageCourseRow>)
                     }
                 </tbody>
             </table>
