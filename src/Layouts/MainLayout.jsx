3f1381b3b13ede323fbb6bcd9add2f45dad9b313
soponalways <soponalways@gmail.com>
2025-08-10 01:24:28 +0600

    Navbar Position sticky and Product card Update

--- src/Layouts/MainLayout.jsx
+++ src/Layouts/MainLayout.jsx
@@ -6,14 +6,14 @@ import Footer from '../Pages/MainLayout/Shared/Footer';
 
 const MainLayout = () => {
     return (
-        <div className='container mx-auto'>
-            <header>
+        <div>
+            <header className='sticky top-0 z-10  bg-base-200 w-full mb-2 md:mb-4 lg:mb-5'>
                 <NavBar></NavBar>
             </header>
-            <main className='min-h-60 md:min-h-72 lg:min-h-96'>
+            <main className='min-h-60 md:min-h-72 lg:min-h-96 container mx-auto'>
                 <Outlet></Outlet>
             </main>
-            <footer>
+            <footer className='bg-base-200 '>
                 <Footer></Footer>
             </footer>
         </div>
